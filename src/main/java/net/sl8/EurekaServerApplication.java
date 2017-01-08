9a64a6084e074b6e43ac3495d2aea6c4b3fd1740
James Jurach <james.jurach@gmail.com>
2017-01-08 11:34:32 -0600

    add full import from start.spring.io

---
+++ src/main/java/net/sl8/EurekaServerApplication.java
@@ -0,0 +1,12 @@
+package net.sl8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
